c54842af198bcfa032e2224841e57eab00d47626
raampandy <raamkumar.pandy@gmail.com>
2026-03-15 19:09:30 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, Response
 import requests
 from twilio.twiml.messaging_response import MessagingResponse
 from urllib.parse import parse_qs
+from datetime import date
 import os
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -35,13 +36,23 @@ def get_arrivals(stop_configs):
         return '\n'.join(results)
     except Exception as e:
         return 'Error: ' + str(e)
-ECF_API_SEARCH = 'https://rating.englishchess.org.uk/v2/new/api.php?v2/players/fuzzy_name/'
-ECF_API_RATING = 'https://rating.englishchess.org.uk/v2/new/api.php?v2/ratings/S/'
-
+ECF_SEARCH = 'https://rating.englishchess.org.uk/v2/new/api.php?v2/players/fuzzy_name/'
+ECF_RATING = 'https://rating.englishchess.org.uk/v2/new/api.php?v2/ratings/'
+def get_rating_for_code(ecf_code, domain):
+    today = str(date.today())
+    url = ECF_RATING + domain + '/' + ecf_code + '/' + today
+    try:
+        r = requests.get(url, timeout=10)
+        data = r.json()
+        if data.get('success'):
+            return str(data.get('revised_rating', 'N/A'))
+        return 'N/A'
+    except:
+        return 'N/A'
 def get_chess_rating(player_name):
     search_name = player_name.replace(' ', '+')
     try:
-        r = requests.get(ECF_API_SEARCH + search_name, timeout=10)
+        r = requests.get(ECF_SEARCH + search_name, timeout=10)
         data = r.json()
         players = data.get('players', [])
         if not players:
@@ -51,26 +62,8 @@ def get_chess_rating(player_name):
             name = p.get('full_name', 'Unknown')
             ecf_code = p.get('ECF_code', '')
             club = p.get('club_name', 'No club listed')
-            # Get actual rating using ECF code
-            standard = 'N/A'
-            rapid = 'N/A'
-            if ecf_code:
-                try:
-                    r2 = requests.get(ECF_API_RATING + ecf_code, timeout=10)
-                    rating_data = r2.json()
-                    ratings = rating_data.get('ratings', [])
-                    if ratings:
-                        standard = str(ratings[0].get('revised_rating', 'N/A'))
-                except:
-                    pass
-                try:
-                    r3 = requests.get(ECF_API_RATING.replace('/S/', '/R/') + ecf_code, timeout=10)
-                    rapid_data = r3.json()
-                    rapid_ratings = rapid_data.get('ratings', [])
-                    if rapid_ratings:
-                        rapid = str(rapid_ratings[0].get('revised_rating', 'N/A'))
-                except:
-                    pass
+            standard = get_rating_for_code(ecf_code, 'S')
+            rapid = get_rating_for_code(ecf_code, 'R')
             return 'Chess: ' + name + '\nStandard: ' + standard + '\nRapid: ' + rapid + '\nClub: ' + club
         else:
             lines = ['Multiple players found. Try LASTNAME FIRSTNAME:']
@@ -100,7 +93,7 @@ def sms_reply():
     if body_upper in STOPS:
         message_text = get_arrivals(STOPS[body_upper])
     elif body_upper in ('HELP', ''):
-        message_text = 'Bus times: Text HOME, BACK, WOOD or WILSON\nChess rating: Text a player name e.g. Magnus Carlsen'
+        message_text = 'Bus times: Text HOME, BACK, WOOD or WILSON\nChess rating: Text LASTNAME FIRSTNAME e.g. Kennedy Aden'
     else:
         message_text = get_chess_rating(body)
     resp.message(message_text)
@@ -108,3 +101,4 @@ def sms_reply():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
+```
